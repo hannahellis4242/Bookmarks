2f5f59e7154673a0cbe480a72c90e74cac446689
Hannah Ellis <hannahellis4242@gmail.com>
2022-03-16 23:36:21 +0000

    sorting out querys for tags

--- bookmarks/src/handlers/tagInfoHandler.ts
+++ bookmarks/src/handlers/tagInfoHandler.ts
@@ -15,12 +15,26 @@ const queryForLink = (link: string) => {
   where link=(select link.id from link where link.url='${link}')`;
 };
 
+const queryForUser = (name: string) => {
+  return `select  bookmark.id,link.url as link,tag.label as tag from bookmark
+  inner join link on bookmark.link = link.id
+  inner join tag on bookmark.tag = tag.id
+  where bookmark.user = (select (id) from user where user.name = '${name}')`;
+};
+
 const tagInfoHandler: RequestHandler = (req, res) => {
   if (req.query.link) {
     //wish to find the tags for a particular link
     queryDB(queryForLink(req.query.link.toString()))
       .then((results) => res.status(200).json(results))
       .catch((err) => res.status(500).json(err));
+  } else if (req.query.user) {
+    //wish to find the tags for a particular user
+    queryDB(queryForUser(req.query.user.toString()))
+      .then((results) => res.status(200).json(results))
+      .catch((err) => res.status(500).json(err));
+  } else {
+    res.status(500).json({ err: "no query" });
   }
 };
 
