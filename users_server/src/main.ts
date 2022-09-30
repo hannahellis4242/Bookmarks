abf466a7ce7f2e7a398bcf6e4e3960cb8e2f2f2d
Hannah Ellis <hannah.ellis@ten10.com>
2022-09-30 12:56:41 +0100

    making login work

--- users_server/src/main.ts
+++ users_server/src/main.ts
@@ -1,32 +1,65 @@
 import express from "express";
 import { join } from "path";
 import { runHTTPSServer } from "./server/createHTTPSSever";
-import { runWithRedirect } from "./server/runWithRedirect";
+import bcrypt from "bcrypt";
 
 interface User {
   name: string;
   password: string;
 }
+interface Result<T> {
+  success: boolean;
+  reason?: string;
+  data?: T;
+}
+const createSuccess = <T>(data: T): Result<T> => {
+  return { success: true, data };
+};
+const createFail = (reason: string): Result<undefined> => {
+  return { success: false, reason };
+};
 const users: User[] = [];
 const app = express();
 
 app.get("/users", (_, res) => {
-  res.json(users);
+  res.json(createSuccess(users));
 });
 app.post("/users", (req, res) => {
   const name = req.query.name;
   const password = req.query.password;
-  console.log(name, ",", password);
   if (name && password) {
     const contains = users.some((user) => user.name === name);
     if (contains) {
-      res.json({ success: false, reason: "user already exists" });
+      res.json(createFail("user already exists"));
     } else {
-      users.push({ name: name.toString(), password: password.toString() });
-      res.json({ success: true, user: name });
+      bcrypt.hash(password.toString(), 12, (err, hash) => {
+        if (err) {
+          res.json(createFail("hash fail : " + err.message));
+        } else {
+          users.push({ name: name.toString(), password: hash });
+          res.json(createSuccess({ user: name }));
+        }
+      });
     }
   } else {
-    res.json({ success: false, reason: "invalid body" });
+    res.json(createFail("invalid parameters"));
+  }
+});
+app.get("/users/validate", async (req, res) => {
+  const name = req.query.name;
+  const password = req.query.password;
+  if (name && password) {
+    const entry = users.find((user) => user.name === name);
+    if (entry) {
+      const matches = await bcrypt.compare(password.toString(), entry.password);
+      if (matches) {
+        res.json(createSuccess({ user: name, valid: true }));
+      } else {
+        res.json(createFail("invalid user"));
+      }
+    } else {
+      res.json(createFail("user does not exist"));
+    }
   }
 });
 app.all("*", (_, res) => {
